1c4c609865f834f09106e934c85350a9ea276b6d
Karol Sójko <karolsojko@protonmail.com>
2022-06-20 12:50:10 +0200

    fix: add default value for db migrations path

--- packages/scheduler/src/Bootstrap/DataSource.ts
+++ packages/scheduler/src/Bootstrap/DataSource.ts
@@ -35,7 +35,7 @@ export const AppDataSource = new DataSource({
     removeNodeErrorCount: 10,
   },
   entities: [Job, Predicate],
-  migrations: [env.get('DB_MIGRATIONS_PATH')],
+  migrations: [env.get('DB_MIGRATIONS_PATH', true) ?? 'dist/migrations/*.js'],
   migrationsRun: true,
   logging: <LoggerOptions>env.get('DB_DEBUG_LEVEL'),
 })
