19455ba6a7d84a389830c728c3dfea550b156985
Karol Sójko <karolsojko@protonmail.com>
2022-10-04 13:13:56 +0200

    feat(analytics): add new statistics measures for income

--- packages/analytics/src/Domain/Statistics/StatisticsMeasure.ts
+++ packages/analytics/src/Domain/Statistics/StatisticsMeasure.ts
@@ -1,5 +1,13 @@
 export enum StatisticsMeasure {
   Income = 'income',
+  PlusSubscriptionInitialMonthlyPaymentsIncome = 'plus-subscription-initial-monthly-payments-income',
+  ProSubscriptionInitialMonthlyPaymentsIncome = 'pro-subscription-initial-monthly-payments-income',
+  PlusSubscriptionInitialAnnualPaymentsIncome = 'plus-subscription-initial-annual-payments-income',
+  ProSubscriptionInitialAnnualPaymentsIncome = 'pro-subscription-initial-annual-payments-income',
+  PlusSubscriptionRenewingMonthlyPaymentsIncome = 'plus-subscription-renewing-monthly-payments-income',
+  ProSubscriptionRenewingMonthlyPaymentsIncome = 'pro-subscription-renewing-monthly-payments-income',
+  PlusSubscriptionRenewingAnnualPaymentsIncome = 'plus-subscription-renewing-annual-payments-income',
+  ProSubscriptionRenewingAnnualPaymentsIncome = 'pro-subscription-renewing-annual-payments-income',
   SubscriptionLength = 'subscription-length',
   RegistrationLength = 'registration-length',
   RegistrationToSubscriptionTime = 'registration-to-subscription-time',
