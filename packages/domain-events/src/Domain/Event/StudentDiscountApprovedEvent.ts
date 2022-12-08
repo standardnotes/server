47d2012b3d96eddf5f6304f158659dc764f9b1c4
Karol Sójko <karolsojko@protonmail.com>
2022-12-08 14:47:33 +0100

    fix(domain-events): remove unused event

--- packages/domain-events/src/Domain/Event/StudentDiscountApprovedEvent.ts
+++
@@ -1,8 +0,0 @@
-import { DomainEventInterface } from './DomainEventInterface'
-
-import { StudentDiscountApprovedEventPayload } from './StudentDiscountApprovedEventPayload'
-
-export interface StudentDiscountApprovedEvent extends DomainEventInterface {
-  type: 'STUDENT_DISCOUNT_APPROVED'
-  payload: StudentDiscountApprovedEventPayload
-}
