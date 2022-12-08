3a12f5c1c40ab6cb236b963bad2a987bacef55e4
Karol Sójko <karolsojko@protonmail.com>
2022-12-08 10:10:55 +0100

    feat(domain-events): remove unused account reset requested event

--- packages/domain-events/src/Domain/Event/AccountResetRequestedEvent.ts
+++
@@ -1,8 +0,0 @@
-import { DomainEventInterface } from './DomainEventInterface'
-
-import { AccountResetRequestedEventPayload } from './AccountResetRequestedEventPayload'
-
-export interface AccountResetRequestedEvent extends DomainEventInterface {
-  type: 'ACCOUNT_RESET_REQUESTED'
-  payload: AccountResetRequestedEventPayload
-}
--- packages/domain-events/src/Domain/Event/AccountResetRequestedEventPayload.ts
+++
@@ -1,4 +0,0 @@
-export interface AccountResetRequestedEventPayload {
-  resetRequestToken: string
-  userEmail: string
-}
