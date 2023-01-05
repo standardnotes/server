db0baf92f1336071a8602a1a20ba6439f10028e6
Karol Sójko <karolsojko@protonmail.com>
2023-01-05 14:33:27 +0100

    fix(auth): return type to include user

--- packages/auth/src/Infra/Http/Response/SignInWithRecoveryCodesResponseBody.ts
+++ packages/auth/src/Infra/Http/Response/SignInWithRecoveryCodesResponseBody.ts
@@ -3,4 +3,9 @@ import { KeyParamsData, SessionBody } from '@standardnotes/responses'
 export interface SignInWithRecoveryCodesResponseBody {
   session: SessionBody
   key_params: KeyParamsData
+  user: {
+    uuid: string
+    email: string
+    protocolVersion: string
+  }
 }
