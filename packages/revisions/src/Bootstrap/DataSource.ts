f73129cd7e7d6a9b8a63e5c80284467597557982
Karol Sójko <karolsojko@protonmail.com>
2023-01-17 13:41:46 +0100

    fix: allow to run typeorm in non-replica mode

--- packages/revisions/src/Bootstrap/DataSource.ts
+++ packages/revisions/src/Bootstrap/DataSource.ts
@@ -11,34 +11,45 @@ const maxQueryExecutionTime = env.get('DB_MAX_QUERY_EXECUTION_TIME', true)
   ? +env.get('DB_MAX_QUERY_EXECUTION_TIME', true)
   : 45_000
 
-export const AppDataSource = new DataSource({
-  type: 'mysql',
-  charset: 'utf8mb4',
-  supportBigNumbers: true,
-  bigNumberStrings: false,
-  maxQueryExecutionTime,
-  replication: {
-    master: {
-      host: env.get('DB_HOST'),
+const inReplicaMode = env.get('DB_REPLICA_HOST', true) ? true : false
+
+const replicationConfig = {
+  master: {
+    host: env.get('DB_HOST'),
+    port: parseInt(env.get('DB_PORT')),
+    username: env.get('DB_USERNAME'),
+    password: env.get('DB_PASSWORD'),
+    database: env.get('DB_DATABASE'),
+  },
+  slaves: [
+    {
+      host: env.get('DB_REPLICA_HOST', true),
       port: parseInt(env.get('DB_PORT')),
       username: env.get('DB_USERNAME'),
       password: env.get('DB_PASSWORD'),
       database: env.get('DB_DATABASE'),
     },
-    slaves: [
-      {
-        host: env.get('DB_REPLICA_HOST'),
-        port: parseInt(env.get('DB_PORT')),
-        username: env.get('DB_USERNAME'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
-      },
-    ],
-    removeNodeErrorCount: 10,
-    restoreNodeTimeout: 5,
-  },
+  ],
+  removeNodeErrorCount: 10,
+  restoreNodeTimeout: 5,
+}
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  charset: 'utf8mb4',
+  supportBigNumbers: true,
+  bigNumberStrings: false,
+  maxQueryExecutionTime,
+  replication: inReplicaMode ? replicationConfig : undefined,
+  host: inReplicaMode ? undefined : env.get('DB_HOST'),
+  port: inReplicaMode ? undefined : parseInt(env.get('DB_PORT')),
+  username: inReplicaMode ? undefined : env.get('DB_USERNAME'),
+  password: inReplicaMode ? undefined : env.get('DB_PASSWORD'),
+  database: inReplicaMode ? undefined : env.get('DB_DATABASE'),
   entities: [TypeORMRevision],
   migrations: [env.get('DB_MIGRATIONS_PATH', true) ?? 'dist/migrations/*.js'],
   migrationsRun: true,
   logging: <LoggerOptions>env.get('DB_DEBUG_LEVEL'),
 })
+
+export const AppDataSource = dataSource
