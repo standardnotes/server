b3e85880407b0adfd254449419e2652897083ae2
moughxyz <mo@standardnotes.com>
2023-01-19 16:19:28 -0600

    Revert "Revert "feat: include roles in offline features request (#418)""
    This reverts commit f20ee68f504449b6ff37748c4d7f83c08bb4039d.

--- packages/auth/src/Domain/Feature/FeatureServiceInterface.ts
+++ packages/auth/src/Domain/Feature/FeatureServiceInterface.ts
@@ -1,8 +1,9 @@
 import { FeatureDescription } from '@standardnotes/features'
 
+import { Role } from '../Role/Role'
 import { User } from '../User/User'
 
 export interface FeatureServiceInterface {
   getFeaturesForUser(user: User): Promise<Array<FeatureDescription>>
-  getFeaturesForOfflineUser(email: string): Promise<FeatureDescription[]>
+  getFeaturesForOfflineUser(email: string): Promise<{ features: FeatureDescription[]; roles: Role[] }>
 }
