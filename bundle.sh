8216fe5e361b33fd7b2cefefe6c009eb0aac49ec
Karol Sójko <karolsojko@protonmail.com>
2023-01-30 09:52:57 +0100

    chore: add script to build a local version of a service

---
+++ bundle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SERVICE=$1 && shift 1
+
+if [ "$SERVICE" == "" ]; then
+  echo "Please input a service name as parameter"
+
+  exit 1
+fi
+
+echo "Bundling and building local docker image for service: $SERVICE"
+
+rm -rf $TMPDIR/bundle-$SERVICE/
+mkdir -p $TMPDIR/bundle-$SERVICE
+
+yarn workspace @standardnotes/$SERVICE-server bundle --no-compress --output-directory $TMPDIR/bundle-$SERVICE
+
+docker build $TMPDIR/bundle-$SERVICE -f $TMPDIR/bundle-$SERVICE/packages/$SERVICE/Dockerfile -t standardnotes/$SERVICE:local
