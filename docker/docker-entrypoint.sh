42233fa7561157d2f24b71d385251b7d6eeccae2
Karol Sójko <karolsojko@protonmail.com>
2023-02-03 17:44:30 +0100

    auth & auth-worker wip

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 # Setup environment variables
 
 printenv | grep API_GATEWAY_ | sed 's/API_GATEWAY_//g' > /opt/server/packages/api-gateway/.env
+printenv | grep AUTH_SERVER_ | sed 's/AUTH_SERVER_//g' > /opt/server/packages/auth/.env
 
 # Run supervisor
 
