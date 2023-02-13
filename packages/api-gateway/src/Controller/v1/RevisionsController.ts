f970eb0ecaf27b08be9c1d9591e4da63a5054e7b
Karol Sójko <karolsojko@protonmail.com>
2023-02-13 09:48:54 +0100

    fix(api-gateway): add client update response on v1 revision endpoints

--- packages/api-gateway/src/Controller/v1/RevisionsController.ts
+++ packages/api-gateway/src/Controller/v1/RevisionsController.ts
@@ -1,35 +1,41 @@
-import { Request, Response } from 'express'
-import { inject } from 'inversify'
-import { BaseHttpController, controller, httpDelete, httpGet } from 'inversify-express-utils'
+import { BaseHttpController, controller, httpDelete, httpGet, results } from 'inversify-express-utils'
 import TYPES from '../../Bootstrap/Types'
-import { HttpServiceInterface } from '../../Service/Http/HttpServiceInterface'
 
 @controller('/v1/items/:item_id/revisions', TYPES.AuthMiddleware)
 export class RevisionsController extends BaseHttpController {
-  constructor(@inject(TYPES.HTTPService) private httpService: HttpServiceInterface) {
-    super()
-  }
-
   @httpGet('/')
-  async getRevisions(request: Request, response: Response): Promise<void> {
-    await this.httpService.callSyncingServer(request, response, `items/${request.params.item_id}/revisions`)
+  async getRevisions(): Promise<results.JsonResult> {
+    return this.json(
+      {
+        error: {
+          message: 'Please update your client application.',
+        },
+      },
+      410,
+    )
   }
 
   @httpGet('/:id')
-  async getRevision(request: Request, response: Response): Promise<void> {
-    await this.httpService.callSyncingServer(
-      request,
-      response,
-      `items/${request.params.item_id}/revisions/${request.params.id}`,
+  async getRevision(): Promise<results.JsonResult> {
+    return this.json(
+      {
+        error: {
+          message: 'Please update your client application.',
+        },
+      },
+      410,
     )
   }
 
   @httpDelete('/:id')
-  async deleteRevision(request: Request, response: Response): Promise<void> {
-    await this.httpService.callSyncingServer(
-      request,
-      response,
-      `items/${request.params.item_id}/revisions/${request.params.id}`,
+  async deleteRevision(): Promise<results.JsonResult> {
+    return this.json(
+      {
+        error: {
+          message: 'Please update your client application.',
+        },
+      },
+      410,
     )
   }
 }
