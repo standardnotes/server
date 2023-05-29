a6234b522512ec09325d11629469611168b3ba8c
Karol Sójko <karolsojko@proton.me>
2023-05-29 13:30:15 +0200

    fix: import for legacy controller

--- packages/api-gateway/bin/server.ts
+++ packages/api-gateway/bin/server.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata'
 
 import 'newrelic'
 
-import '../src/Infra/InversifyExpress/LegacyController'
+import '../src/Controller/LegacyController'
 import '../src/Controller/HealthCheckController'
 
 import '../src/Controller/v1/SessionsController'
