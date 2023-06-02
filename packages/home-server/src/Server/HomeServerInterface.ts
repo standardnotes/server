58ab410b0afb1d811247cd65b2585d06f9c8807a
Karol Sójko <karolsojko@proton.me>
2023-06-02 13:27:41 +0200

    fix(home-server): remove redundant restart method

--- packages/home-server/src/Server/HomeServerInterface.ts
+++ packages/home-server/src/Server/HomeServerInterface.ts
@@ -3,7 +3,6 @@ import { HomeServerConfiguration } from './HomeServerConfiguration'
 export interface HomeServerInterface {
   start(configuration?: HomeServerConfiguration): Promise<void>
   stop(): Promise<void>
-  restart(): Promise<void>
   isRunning(): Promise<boolean>
   logs(): NodeJS.ReadableStream
 }
