e38a16404c1b335ab0ef4d8383f6f644e51934ad
Karol Sójko <karolsojko@proton.me>
2023-06-22 12:37:19 +0200

    fix(home-server): listening on log stream

--- packages/home-server/bin/server.ts
+++ packages/home-server/bin/server.ts
@@ -6,10 +6,12 @@ Promise.resolve(homeServer.start({ dataDirectoryPath: `${__dirname}/../data` }))
   .then(() => {
     const logStream = homeServer.logs()
 
-    logStream.on('data', (chunk: Buffer) => {
-      // eslint-disable-next-line no-console
-      console.log(chunk.toString())
-    })
+    if (logStream !== undefined) {
+      logStream.on('data', (chunk: Buffer) => {
+        // eslint-disable-next-line no-console
+        console.log(chunk.toString())
+      })
+    }
   })
   .catch((error) => {
     // eslint-disable-next-line no-console
