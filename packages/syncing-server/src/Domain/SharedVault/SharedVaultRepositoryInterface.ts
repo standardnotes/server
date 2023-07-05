b0d780cccfa19f21fcae54e01ebae6826b28ef73
Karol Sójko <karolsojko@proton.me>
2023-07-05 10:34:25 +0200

    feat: add getting shared vaults for a user

--- packages/syncing-server/src/Domain/SharedVault/SharedVaultRepositoryInterface.ts
+++ packages/syncing-server/src/Domain/SharedVault/SharedVaultRepositoryInterface.ts
@@ -3,6 +3,7 @@ import { SharedVault } from './SharedVault'
 
 export interface SharedVaultRepositoryInterface {
   findByUuid(uuid: Uuid): Promise<SharedVault | null>
+  findByUuids(uuids: Uuid[], lastSyncTime?: number): Promise<SharedVault[]>
   save(sharedVault: SharedVault): Promise<void>
   remove(sharedVault: SharedVault): Promise<void>
 }
---
+++ packages/syncing-server/src/Domain/UseCase/GetSharedVaults/GetSharedVaultsDTO.ts
@@ -0,0 +1,4 @@
+export interface GetSharedVaultsDTO {
+  userUuid: string
+  lastSyncTime?: number
+}
