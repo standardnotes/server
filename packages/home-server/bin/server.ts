f7471119e1c4107dd36d37dcda4660870931fc83
Karol Sójko <karolsojko@proton.me>
2023-07-27 12:00:40 +0200

    fix: add logging error stack on home server failure

--- packages/home-server/bin/server.ts
+++ packages/home-server/bin/server.ts
@@ -6,16 +6,21 @@ const homeServer = new HomeServer()
 const env: Env = new Env()
 env.load()
 
-Promise.resolve(
-  homeServer.start({
-    dataDirectoryPath: `${__dirname}/../data`,
-    logStreamCallback: (chunk: Buffer) => {
-      // eslint-disable-next-line no-console
-      console.log(chunk.toString())
-    },
-    environment: env.getAll(),
-  }),
-).catch((error) => {
+try {
+  Promise.resolve(
+    homeServer.start({
+      dataDirectoryPath: `${__dirname}/../data`,
+      logStreamCallback: (chunk: Buffer) => {
+        // eslint-disable-next-line no-console
+        console.log(chunk.toString())
+      },
+      environment: env.getAll(),
+    }),
+  ).catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error(`Could not start server: ${error.message}`)
+  })
+} catch (error) {
   // eslint-disable-next-line no-console
-  console.log(`Could not start server: ${error.message}`)
-})
+  console.error((error as Error).stack)
+}
