e9bba6fd3acfde62c3063160ba3ec3aa83c45b31
Karol Sójko <karolsojko@gmail.com>
2023-08-01 18:15:16 +0200

    feat(syncing-server): remove legacy privileges items (#679)
    Co-authored-by: Karol Sójko <karolsojko@proton.me>

---
+++ packages/syncing-server/migrations/sqlite/1690901030484-delete_privileges.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class DeletePrivileges1690901030484 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const itemsWithPrivilegesContentTypeQueryResult = await queryRunner.manager.query(
+      'SELECT COUNT(*) as count FROM items i WHERE i.content_type = "SN|Privileges"',
+    )
+    const itemsWithPrivilegesContentTypeCount = +itemsWithPrivilegesContentTypeQueryResult[0].count
+
+    const batchSize = 1_000
+    const batchCount = Math.ceil(itemsWithPrivilegesContentTypeCount / batchSize)
+
+    for (let batchIndex = 0; batchIndex < batchCount; batchIndex++) {
+      await queryRunner.manager.query(`DELETE FROM items WHERE content_type = "SN|Privileges" LIMIT ${batchSize}`)
+    }
+  }
+
+  public async down(): Promise<void> {
+    return
+  }
+}
