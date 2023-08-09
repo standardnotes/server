1c3ff526b7c4885f71f019f6c01142f522a6f8ad
Karol Sójko <karolsojko@gmail.com>
2023-08-09 18:00:49 +0200

    Revert "Revert "feat(syncing-server): notify shared vault users upon file uploads or removals (#692)""
    This reverts commit d261c81cd0bdbb9001c8589224f007ed2d338903.

--- packages/syncing-server/src/Bootstrap/Container.ts
+++ packages/syncing-server/src/Bootstrap/Container.ts
@@ -157,6 +157,7 @@ import { SharedVaultSnjsFilter } from '../Domain/Item/SaveRule/SharedVaultSnjsFi
 import { UpdateStorageQuotaUsedInSharedVault } from '../Domain/UseCase/SharedVaults/UpdateStorageQuotaUsedInSharedVault/UpdateStorageQuotaUsedInSharedVault'
 import { SharedVaultFileUploadedEventHandler } from '../Domain/Handler/SharedVaultFileUploadedEventHandler'
 import { SharedVaultFileRemovedEventHandler } from '../Domain/Handler/SharedVaultFileRemovedEventHandler'
+import { AddNotificationsForUsers } from '../Domain/UseCase/Messaging/AddNotificationsForUsers/AddNotificationsForUsers'
 
 export class ContainerConfigLoader {
   private readonly DEFAULT_CONTENT_SIZE_TRANSFER_LIMIT = 10_000_000
@@ -563,6 +564,14 @@ export class ContainerConfigLoader {
         new AddNotificationForUser(container.get(TYPES.Sync_NotificationRepository), container.get(TYPES.Sync_Timer)),
       )
     container
+      .bind<AddNotificationsForUsers>(TYPES.Sync_AddNotificationsForUsers)
+      .toConstantValue(
+        new AddNotificationsForUsers(
+          container.get<SharedVaultUserRepositoryInterface>(TYPES.Sync_SharedVaultUserRepository),
+          container.get<AddNotificationForUser>(TYPES.Sync_AddNotificationForUser),
+        ),
+      )
+    container
       .bind<RemoveNotificationsForUser>(TYPES.Sync_RemoveNotificationsForUser)
       .toConstantValue(new RemoveNotificationsForUser(container.get(TYPES.Sync_NotificationRepository)))
     container
@@ -831,6 +840,7 @@ export class ContainerConfigLoader {
       .toConstantValue(
         new SharedVaultFileUploadedEventHandler(
           container.get<UpdateStorageQuotaUsedInSharedVault>(TYPES.Sync_UpdateStorageQuotaUsedInSharedVault),
+          container.get<AddNotificationsForUsers>(TYPES.Sync_AddNotificationsForUsers),
           container.get<winston.Logger>(TYPES.Sync_Logger),
         ),
       )
@@ -839,6 +849,7 @@ export class ContainerConfigLoader {
       .toConstantValue(
         new SharedVaultFileRemovedEventHandler(
           container.get<UpdateStorageQuotaUsedInSharedVault>(TYPES.Sync_UpdateStorageQuotaUsedInSharedVault),
+          container.get<AddNotificationsForUsers>(TYPES.Sync_AddNotificationsForUsers),
           container.get<winston.Logger>(TYPES.Sync_Logger),
         ),
       )
