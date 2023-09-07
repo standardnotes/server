27ad8e6959aeb9c2f11ee0fecba37514bf91dda8
Karol Sójko <karolsojko@gmail.com>
2023-09-07 17:44:53 +0200

    fix(revisions): waiting for syncing-server on self-hosted setup

--- packages/revisions/supervisor/supervisor-server.sh
+++ packages/revisions/supervisor/supervisor-server.sh
@@ -2,6 +2,5 @@
 
 set -euo pipefail
 
-sh supervisor/wait-for.sh $DB_HOST $DB_PORT
-sh supervisor/wait-for.sh $REDIS_HOST $REDIS_PORT
+sh supervisor/wait-for.sh localhost $SYNCING_SERVER_PORT
 node docker/entrypoint-server.js
