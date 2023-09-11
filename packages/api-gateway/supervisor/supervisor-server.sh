0ab47013f210dca7aa404966798011947fb5c362
Karol Sójko <karolsojko@gmail.com>
2023-09-11 12:00:43 +0200

    fix(api-gateway): awaiting for other services to start

--- packages/api-gateway/supervisor/supervisor-server.sh
+++ packages/api-gateway/supervisor/supervisor-server.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+sh supervisor/wait-for.sh localhost $AUTH_SERVER_PORT
+sh supervisor/wait-for.sh localhost $FILES_SERVER_PORT
+sh supervisor/wait-for.sh localhost $REVISIONS_SERVER_PORT
 sh supervisor/wait-for.sh localhost $SYNCING_SERVER_PORT
 node docker/entrypoint-server.js
