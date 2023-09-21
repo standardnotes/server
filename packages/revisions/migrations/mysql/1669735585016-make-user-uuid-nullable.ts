157b3a9675487b57557f94e4f7fcd874b7ef5e58
Karol Sójko <karolsojko@gmail.com>
2023-09-21 11:34:03 +0200

    fix(revisions): rename revisions table to all users stuck mid-migration process - fixes #836

--- packages/revisions/migrations/mysql/1669735585016-make-user-uuid-nullable.ts
+++ packages/revisions/migrations/mysql/1669735585016-make-user-uuid-nullable.ts
@@ -4,10 +4,22 @@ export class makeUserUuidNullable1669735585016 implements MigrationInterface {
   name = 'makeUserUuidNullable1669735585016'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await this.renameRevisionsTable(queryRunner)
+
     await queryRunner.query('ALTER TABLE `revisions_revisions` CHANGE `user_uuid` `user_uuid` varchar(36) NULL')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE `revisions_revisions` CHANGE `user_uuid` `user_uuid` varchar(36) NOT NULL')
+  }
+
+  private async renameRevisionsTable(queryRunner: QueryRunner) {
+    const revisionsTableExistsQueryResult = await queryRunner.manager.query(
+      'SELECT COUNT(*) as count FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = "revisions"',
+    )
+    const revisionsTableExists = revisionsTableExistsQueryResult[0].count === 1
+    if (revisionsTableExists) {
+      await queryRunner.query('RENAME TABLE `revisions` TO `revisions_revisions`')
+    }
   }
 }
