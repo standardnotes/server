581211beb141417e10966237c86323efb44aa03c
Karol Sójko <karolsojko@gmail.com>
2023-09-21 11:04:42 +0200

    add designated survivor property to http representation

--- packages/syncing-server/src/Mapping/Http/SharedVaultUserHttpRepresentation.ts
+++ packages/syncing-server/src/Mapping/Http/SharedVaultUserHttpRepresentation.ts
@@ -3,6 +3,7 @@ export interface SharedVaultUserHttpRepresentation {
   shared_vault_uuid: string
   user_uuid: string
   permission: string
+  is_designated_survivor: boolean
   created_at_timestamp: number
   updated_at_timestamp: number
 }
