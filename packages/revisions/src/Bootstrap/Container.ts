76ae6f5a882a82ab5f635452e3bc7b2b16709531
Karol Sójko <karolsojko@gmail.com>
2023-10-09 19:13:43 +0200

    Revert "Revert "fix: setting parent span on workers""
    This reverts commit 3fc07a5b60c26b583efd88e8a80d4c4321e71efb.

--- packages/revisions/src/Bootstrap/Container.ts
+++ packages/revisions/src/Bootstrap/Container.ts
@@ -533,7 +533,11 @@ export class ContainerConfigLoader {
         .toConstantValue(
           isConfiguredForHomeServerOrSelfHosting
             ? new SQSEventMessageHandler(eventHandlers, container.get(TYPES.Revisions_Logger))
-            : new SQSOpenTelemetryEventMessageHandler(eventHandlers, container.get(TYPES.Revisions_Logger)),
+            : new SQSOpenTelemetryEventMessageHandler(
+                ServiceIdentifier.NAMES.RevisionsWorker,
+                eventHandlers,
+                container.get(TYPES.Revisions_Logger),
+              ),
         )
 
       container
