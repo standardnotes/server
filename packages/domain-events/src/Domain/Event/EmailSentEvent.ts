f6ec8626e57f1d93210aaf9fae4c15ae032aca8e
Karol Sójko <karolsojko@gmail.com>
2023-11-13 10:58:01 +0100

    fix(domain-events): remove unused event

--- packages/domain-events/src/Domain/Event/EmailSentEvent.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEventInterface } from './DomainEventInterface'
-import { EmailSentEventPayload } from './EmailSentEventPayload'
-
-export interface EmailSentEvent extends DomainEventInterface {
-  type: 'EMAIL_SENT'
-  payload: EmailSentEventPayload
-}
