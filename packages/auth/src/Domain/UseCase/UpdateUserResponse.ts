81c548f5e2923f3832725505e369c9086d493c88
Karol Sójko <karolsojko@gmail.com>
2024-05-21 07:19:43 +0200

    remove stale files

--- packages/auth/src/Domain/UseCase/UpdateUserResponse.ts
+++
@@ -1,7 +0,0 @@
-import { AuthResponse20161215 } from '../Auth/AuthResponse20161215'
-import { AuthResponse20200115 } from '../Auth/AuthResponse20200115'
-
-export type UpdateUserResponse = {
-  success: boolean
-  authResponse?: AuthResponse20161215 | AuthResponse20200115
-}
